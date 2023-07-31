729efb425e22cd75c166f06cdb15e4d4ea7c4755
Devesh Kakad <deveshkakad2016@gmail.com>
2023-07-31 23:00:40 +0530

    html templates and some modification of routes

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -50,3 +50,15 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 		StringMap: stringMap,
 	})
 }
+
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "make-reservation.page.html", &models.TemplateData{})
+}
+
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "generals.page.html", &models.TemplateData{})
+}
+
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "majors.page.html", &models.TemplateData{})
+}
