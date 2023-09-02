21d1962bfe9a02a952e4cd9ad70028efda603419
Devesh Kakad <deveshkakad2016@gmail.com>
2023-09-02 14:32:36 +0530

    new go test files for routers

--- cmd/web/setup_test.go
+++ cmd/web/setup_test.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"encoding/gob"
+	"log"
 	"net/http"
 	"os"
 	"testing"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/deveshkakad/bookings-app/internal/handlers"
+	"github.com/deveshkakad/bookings-app/internal/models"
+	"github.com/deveshkakad/bookings-app/internal/render"
 )
 
 func TestMain(m *testing.M) {
@@ -15,3 +23,7 @@ type myHandler struct{}
 func (mh *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 }
+
+//var app config.AppConfig
+//var session *scs.SessionManager
+
--- internal/render/render.go
+++ internal/render/render.go
@@ -16,6 +16,7 @@ import (
 var functions = template.FuncMap{}
 
 var app *config.AppConfig
+var pathToTemplates = "./templates"
 
 // NewTemplates sets the config for the template package
 func NewTemplates(a *config.AppConfig) {
@@ -59,12 +60,17 @@ func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *mod
 
 }
 
+// NewRenderer sets the config for the template package
+func NewRenderer(a *config.AppConfig) {
+	app = a
+}
+
 // CreateTemplateCache creates a template cache as a map
 func CreateTemplateCache() (map[string]*template.Template, error) {
 
 	myCache := map[string]*template.Template{}
 
-	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	pages, err := filepath.Glob(fmt.Sprintf("%s/*.page.tmpl", pathToTemplates))
 	if err != nil {
 		return myCache, err
 	}
