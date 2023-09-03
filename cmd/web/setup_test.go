ee641093cc8c6dc68efba337ae52330b1be9a61d
Devesh Kakad <deveshkakad2016@gmail.com>
2023-09-03 13:35:41 +0530

    running application command writter in a script to make running application easier

--- cmd/web/setup_test.go
+++ cmd/web/setup_test.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"encoding/gob"
-	"log"
 	"net/http"
 	"os"
 	"testing"
-	"time"
-
-	"github.com/alexedwards/scs/v2"
-	"github.com/deveshkakad/bookings-app/internal/handlers"
-	"github.com/deveshkakad/bookings-app/internal/models"
-	"github.com/deveshkakad/bookings-app/internal/render"
 )
 
 func TestMain(m *testing.M) {
@@ -26,4 +18,3 @@ func (mh *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 //var app config.AppConfig
 //var session *scs.SessionManager
-
